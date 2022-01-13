5f68363e17bb5538f0807fe21e2143effffd538f
cares0 <cares00000@gmail.com>
2022-01-13 16:09:30 +0900

    Day16_Ex1Answer

--- src/com/cares/s3/ex1/Ex1Main.java
+++ src/com/cares/s3/ex1/Ex1Main.java
@@ -2,6 +2,8 @@ package com.cares.s3.ex1;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
 import com.cares.s3.ex1.member.MemberDTO;
 import com.cares.s3.ex1.member.MemberData;
@@ -17,10 +19,13 @@ public class Ex1Main {
 		// 회원들의 정보 모음
 		ArrayList<MemberDTO> ar = memberData.init();
 		HashMap<String, StudentDTO> map = (HashMap) studentData.addStudent(ar);		
+
+		Set<String> keys = map.keySet();
+		Iterator<String> it = keys.iterator();
 		
-		for(int i=0;i<map.size();i++) {
-			StudentDTO stu = new StudentDTO();
-			stu = map.get(ar.get(i).getId());
+		while(it.hasNext()) {
+			String key = it.next();
+			StudentDTO stu = map.get(key);
 			
 			System.out.println("ID : " + stu.getId());
 			System.out.println("Name : " + stu.getName());
@@ -30,8 +35,25 @@ public class Ex1Main {
 			System.out.println("Total : " + stu.getTotal());
 			System.out.println("Avg : " + stu.getAvg());
 			System.out.println("================");
-						
 		}
+		
+		
+		
+		
+//		for(int i=0;i<map.size();i++) {
+//			StudentDTO stu = new StudentDTO();
+//			stu = map.get(ar.get(i).getId());
+//			
+//			System.out.println("ID : " + stu.getId());
+//			System.out.println("Name : " + stu.getName());
+//			System.out.println("Kor : " + stu.getKor());
+//			System.out.println("Eng : " + stu.getEng());
+//			System.out.println("Math : " + stu.getMath());
+//			System.out.println("Total : " + stu.getTotal());
+//			System.out.println("Avg : " + stu.getAvg());
+//			System.out.println("================");
+//						
+//		}
 		
 	}
 
