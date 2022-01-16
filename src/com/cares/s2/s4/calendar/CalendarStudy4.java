bf53beebb7cd5c659fe8050dc66fce05d66ad4e4
cares0 <cares00000@gmail.com>
2022-01-17 08:53:20 +0900

    Day17_Calendar

--- src/com/cares/s2/s4/calendar/CalendarStudy4.java
+++ src/com/cares/s2/s4/calendar/CalendarStudy4.java
@@ -9,15 +9,17 @@ public class CalendarStudy4 {
 
 		// 현재 시간
 		Calendar ca = Calendar.getInstance();
+
 		// 태어난 날
 		Calendar ca2 = Calendar.getInstance();
 		ca2.set(2000, 0, 14);
-		System.out.println(ca2.getTime());
-		// 며칠
+
+		// 차이 계산
 		long l1 = ca.getTimeInMillis();
-		long l2 = ca2.getTimeInMillis();
-		
+		long l2 = ca2.getTimeInMillis();		
 		long result = l1 - l2;
+		
+		// 며칠
 		System.out.println(result/(1000*60*60*24));		
 		// 나이
 		System.out.println(result/(1000*60*60*24*365.2425));
